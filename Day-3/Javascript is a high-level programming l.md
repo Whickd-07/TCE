c1aa6e501df7b5c561c3251717b84098fed232f5
Abyan Mohammed <116152478+Whickd-07@users.noreply.github.com>
2023-10-19 18:51:53 +0530

    Add files via upload

---
+++ Day-3/Javascript is a high-level programming l.md
@@ -0,0 +1,27 @@
+Javascript is a high-level programming language or scripting language used to make our websites and web-applications interactive.
+
+Ways to add javascript to HTML:
+-Inline JS
+-Internal JS
+-External JS
+
+Some predefined functions of JS:
+-alert()
+-prompt()
+-console.log()
+-document.write()
+
+Variables are containers used to store data in code.
+Declaration methods:
+-var
+-let
+-const
+
+Rules to write the variables:
+1) Variables start with the letter or $ or _.
+2) Variables include only letter, number, _ and $.
+3) Variables are case-sensitive.
+4) Variables cannot be reserved keywords like for, if, else, etc.
+
+Datatypes:
+Primitive - Number, string, boolean, 
---
+++ Day-3/index.js
@@ -0,0 +1,79 @@
+// console.log("Today is Friday");
+// console.log(3+5);
+
+ document.write("<br> <h1>Javascript is programming language.</h1>");
+
+// variables
+var num1 = 54;
+var num2 = 65;
+num1 = 60
+console.log(num1)
+
+let num = 22;
+num2 = 23;
+console.log(num2);
+
+const num3 = 66;
+// num3 = 28; We can't redeclare the variables
+// 
+console.log(num3);
+
+let a;
+console.log(typeof a);
+let b = null
+console.log(typeof b);
+
+
+// let person = {
+//     name : "<h2><u>Abyan</u></h2>",
+//     age: "21",
+//     gender: "MALE",
+//     mail: "xyz",
+//     Mob: "1234567890"
+// };
+// console.log(person);
+// document.write(person.name);
+
+// let vehicle = ["car","bus","truck","bike"];
+// console.log(vehicle[3]);
+// console.log(vehicle.length);
+
+
+// function mul(num1, num2, num3){
+//     return num1*num2*num3;
+// }
+// document.write(mul(3,4,5));
+
+function greeting(){
+    alert("Hello BSDK");
+};
+
+// let c = 5;
+// let d = "5";
+// console.log(a == b);
+// console.log(a === b);
+
+// let age = 55;
+//  if (age >= 18){
+//     document.write("Elligible for vote");
+//  } else if (age>=18 && age<=40){
+//     document.write("Young");
+//  } else {
+//     document.write("Old");
+//  }
+
+// for (let i=1; i<=10; i++){
+//     console.log(i);
+// }
+
+// DOM Manipulation
+// document.getElementById(id)	Find an element by element id
+// document.getElementsByTagName(name)	Find elements by tag name
+// document.getElementsByClassName(name)	Find elements by class name
+// document.querySelector()
+// document.querySelectorAll()
+
+let el = document.getElementById("head").innerHTML;
+console.log(el);
+
+                
